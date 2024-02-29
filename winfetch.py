ddc0ece6ae88d96f1c793fa66d2948c656b50150
Warrobot10 <132307750+Warrobot10@users.noreply.github.com>
2024-02-29 16:28:25 +1000

    1.0.1
    Minor Formatting

--- winfetch.py
+++ winfetch.py
@@ -28,7 +28,7 @@ gpu_infos = [gpu.name for gpu in computer.Win32_VideoController()]
 
 cpu = cpuinfo.get_cpu_info()["brand_raw"]
 
-current_version = "1.0.0"
+current_version = "1.0.1"
 
 time_now = datetime.datetime.now()
 formatted_date = time_now.strftime("%d-%m-%Y")
@@ -36,22 +36,42 @@ formatted_time = time_now.strftime("%I:%M")
 day = time_now.strftime("%A")
 
 # Print Winfetch status
-cprint(colored("Winfetch version ", selected_theme["header"]) + colored(current_version, selected_theme["value"]))
+cprint(
+    colored("Winfetch version ", selected_theme["header"])
+    + colored(current_version, selected_theme["value"])
+)
 
 # Print Date and time
-cprint(colored("Date: ", selected_theme["header"]) + colored(formatted_date, selected_theme["value"]))
-cprint(colored("Time: ", selected_theme["header"]) + colored(formatted_time, selected_theme["value"]))
-cprint(colored("It is a ", selected_theme["header"]) + colored(day, selected_theme["value"]))
+cprint(
+    colored("Date: ", selected_theme["header"])
+    + colored(formatted_date, selected_theme["value"])
+)
+cprint(
+    colored("Time: ", selected_theme["header"])
+    + colored(formatted_time, selected_theme["value"])
+)
+cprint(
+    colored("It is a ", selected_theme["header"])
+    + colored(day, selected_theme["value"])
+)
 
 # Print System info
 for i, gpu_info in enumerate(gpu_infos, start=1):
-    cprint(colored(f"GPU {i}: ", selected_theme["header"]) + colored(gpu_info, selected_theme["value"]))
+    cprint(
+        colored(f"GPU {i}: ", selected_theme["header"])
+        + colored(gpu_info, selected_theme["value"])
+    )
 
-cprint(colored("CPU: ", selected_theme["header"]) + colored(cpu, selected_theme["value"]))
+cprint(
+    colored("CPU: ", selected_theme["header"]) + colored(cpu, selected_theme["value"])
+)
 
 # Get CPU cores
 cpu_cores = sum(int(cpu.NumberOfCores) for cpu in computer.Win32_Processor())
-cprint(colored("CPU cores: ", selected_theme["header"]) + colored(cpu_cores, selected_theme["value"]))
+cprint(
+    colored("CPU cores: ", selected_theme["header"])
+    + colored(cpu_cores, selected_theme["value"])
+)
 
 end = time.time()
 
