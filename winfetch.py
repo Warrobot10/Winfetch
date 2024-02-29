591bb3a309c0b053e7062ccfa92eaa5bdbebacd9
Warrobot10 <132307750+Warrobot10@users.noreply.github.com>
2024-02-29 16:31:36 +1000

    1.0.2
    Formatting

--- winfetch.py
+++ winfetch.py
@@ -1,23 +1,32 @@
 import json
 import datetime
+import time
 import wmi
 import cpuinfo
 from termcolor import colored, cprint
-import time
 
 
 def load_themes_from_json(file_path):
-    with open(file_path, "r") as file:
+    """
+    Load themes from a JSON file.
+
+    Args:
+        file_path (str): The path to the JSON file containing themes.
+
+    Returns:
+        dict: A dictionary containing the loaded themes.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
 
 # Load themes from JSON file
-themes_file_path = "themes.json"  # Update with your themes file path
-themes = load_themes_from_json(themes_file_path)
+THEMES_FILE_PATH = "themes.json"  # Update with your themes file path
+THEMES = load_themes_from_json(THEMES_FILE_PATH)
 
 # Select a theme
-selected_theme_name = "default"  # Change this to the theme name you want to use
-selected_theme = themes[selected_theme_name]
+SELECTED_THEME_NAME = "default"  # Change this to the theme name you want to use
+SELECTED_THEME = THEMES[SELECTED_THEME_NAME]
 
 start = time.time()
 
@@ -28,7 +37,7 @@ gpu_infos = [gpu.name for gpu in computer.Win32_VideoController()]
 
 cpu = cpuinfo.get_cpu_info()["brand_raw"]
 
-current_version = "1.0.1"
+CURRENT_VERSION = "1.0.1"
 
 time_now = datetime.datetime.now()
 formatted_date = time_now.strftime("%d-%m-%Y")
@@ -37,40 +46,40 @@ day = time_now.strftime("%A")
 
 # Print Winfetch status
 cprint(
-    colored("Winfetch version ", selected_theme["header"])
-    + colored(current_version, selected_theme["value"])
+    colored("Winfetch version ", SELECTED_THEME["header"])
+    + colored(CURRENT_VERSION, SELECTED_THEME["value"])
 )
 
 # Print Date and time
 cprint(
-    colored("Date: ", selected_theme["header"])
-    + colored(formatted_date, selected_theme["value"])
+    colored("Date: ", SELECTED_THEME["header"])
+    + colored(formatted_date, SELECTED_THEME["value"])
 )
 cprint(
-    colored("Time: ", selected_theme["header"])
-    + colored(formatted_time, selected_theme["value"])
+    colored("Time: ", SELECTED_THEME["header"])
+    + colored(formatted_time, SELECTED_THEME["value"])
 )
 cprint(
-    colored("It is a ", selected_theme["header"])
-    + colored(day, selected_theme["value"])
+    colored("It is a ", SELECTED_THEME["header"])
+    + colored(day, SELECTED_THEME["value"])
 )
 
 # Print System info
 for i, gpu_info in enumerate(gpu_infos, start=1):
     cprint(
-        colored(f"GPU {i}: ", selected_theme["header"])
-        + colored(gpu_info, selected_theme["value"])
+        colored(f"GPU {i}: ", SELECTED_THEME["header"])
+        + colored(gpu_info, SELECTED_THEME["value"])
     )
 
 cprint(
-    colored("CPU: ", selected_theme["header"]) + colored(cpu, selected_theme["value"])
+    colored("CPU: ", SELECTED_THEME["header"]) + colored(cpu, SELECTED_THEME["value"])
 )
 
 # Get CPU cores
 cpu_cores = sum(int(cpu.NumberOfCores) for cpu in computer.Win32_Processor())
 cprint(
-    colored("CPU cores: ", selected_theme["header"])
-    + colored(cpu_cores, selected_theme["value"])
+    colored("CPU cores: ", SELECTED_THEME["header"])
+    + colored(cpu_cores, SELECTED_THEME["value"])
 )
 
 end = time.time()
@@ -78,7 +87,7 @@ end = time.time()
 time_taken = end - start
 time_taken_formatted = "{:.2f}".format(time_taken)
 cprint(
-    colored("Elapsed time: ", selected_theme["header"])
-    + colored(time_taken_formatted, selected_theme["value"])
-    + colored("s", selected_theme["value"])
+    colored("Elapsed time: ", SELECTED_THEME["header"])
+    + colored(time_taken_formatted, SELECTED_THEME["value"])
+    + colored("s", SELECTED_THEME["value"])
 )
